27edabea97070894ebba119b087912e0c18f95e2
gfriesen98 <garettf999@gmail.com>
2021-07-01 20:09:22 -0500

    fuck you

--- app.js
+++ app.js
@@ -204,6 +204,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'../build/index.html'));
 })
 
-server.listen(5000, () => {
+server.listen(process.env.PORT || 5000, () => {
   console.log('server running on port 5000');
 });
