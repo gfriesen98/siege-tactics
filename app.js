726c99f0be74b1efcbca60a400eadac5a38eed6c
gfriesen98 <garettf999@gmail.com>
2021-07-01 20:12:30 -0500

    fuck you

--- app.js
+++ app.js
@@ -201,7 +201,7 @@ io.on('connection', (socket) => {
 app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'../build/index.html'));
+  res.sendFile(path.join(__dirname+'/build/index.html'));
 })
 
 server.listen(process.env.PORT || 5000, () => {
